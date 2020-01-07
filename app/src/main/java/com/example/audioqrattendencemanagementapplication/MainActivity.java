b6d223b3deca9ef5234537b56f98a38b538b9d47
Aditya Gupta <34581503+aditya1k2@users.noreply.github.com>
2020-01-07 21:24:01 +0530

    int inta declared for testing

--- app/src/main/java/com/example/audioqrattendencemanagementapplication/MainActivity.java
+++ app/src/main/java/com/example/audioqrattendencemanagementapplication/MainActivity.java
@@ -9,8 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int test_123;
-        int test_1234;
-        int test_12345;
+        int a;
     }
 }
